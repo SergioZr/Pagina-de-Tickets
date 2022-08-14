bdd885a3d2b990612d82709e959353b89292a0c6
Sergio <sergiozacariasramos@gamil.com>
2022-08-13 21:32:14 -0600

    creacion del formulario de tickets

--- app/tickets-soporte/src/Pages/CreateTickets.js
+++ app/tickets-soporte/src/Pages/CreateTickets.js
@@ -26,22 +26,41 @@ export function TicketsUser() {
     <div className="contenedor">
       <h2>¡Crea tu ticket!</h2>
       <form onSubmit={sendlogin}>
+        <h2>Información de Contacto</h2>
+
         <div className="usuario">
           <input type="tel" name="telefono" required onChange={handelChange} />
           <label>Ingresa tu número de teléfono</label>
         </div>
+
         <div className="consulta">
           <legend>¿Desea soporte remoto?</legend>
           <br></br>
-          <label>Si, por favor  
-          <input type="radio" name="consulta" onChange={handelChange} />
+          <label>
+            Si, por favor
+            <input type="radio" name="consulta" onChange={handelChange} />
           </label>
           <br></br>
           <br></br>
-          <label>No, gracias 
-          <input type="radio" name="consulta" onChange={handelChange} />
+          <label>
+            No, gracias
+            <input type="radio" name="consulta" onChange={handelChange} />
           </label>
         </div>
+
+        <br></br>
+
+        <div className="usuario">
+          <input
+            type="text"
+            name="AnyDeskID"
+            required
+            onChange={handelChange}
+          />
+          <label>Ingresa tu ID de AnyDesk</label>
+        </div>
+        <h2>¿Cúal es el problema?</h2>
+
         <button className="iniciar">
           <span></span>
           <span></span>
